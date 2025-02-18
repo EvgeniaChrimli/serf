2211b834ae81c6501281148a74bc6ffdd973eca9
Хримли Евгения <evichkanova@list.ru>
2025-02-18 22:15:51 +0300

    anim

--- scripts/app.js
+++ scripts/app.js
@@ -22,3 +22,28 @@ window.addEventListener("click", (e) => {
     }
   }
 });
+
+const iconsContent = document.querySelector(".content__social");
+const icons = document.querySelectorAll(".content__social-item");
+const text = document.querySelector(".text");
+const infoTitle = document.querySelector(".body__info-title");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  const iconsTop = iconsContent.offsetTop;
+  if (iconsTop > scrollPosition) {
+    icons.forEach((elem) => {
+      elem.classList.add("active-icon");
+    });
+  }
+
+  const textTop = text.offsetTop;
+  if (textTop > scrollPosition) {
+    text.classList.add("active-text");
+  }
+
+  const topTitle = infoTitle.offsetTop;
+  if (topTitle > scrollPosition) {
+    infoTitle.classList.add("info-active");
+  }
+});
